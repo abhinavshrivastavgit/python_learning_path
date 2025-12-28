362e218d467625433672300160e0b1cf9a1ea057
Abhinav Shrivastav <abhinavshrivastavgithub@gmail.com>
2025-12-28 18:40:17 +0530

    Day5: Add inventory_manager.py: implemented list initialization, .append(), and len() functions

---
+++ inventory_manager.py
@@ -0,0 +1,17 @@
+# 1. Defining the List (Our current stock)
+# Notice the square brackets [] and commas ,
+gpu_inventory = ["A100", "H100", "B100"]
+
+print("--- Initial Inventory ---")
+print(gpu_inventory)
+
+# 2. Adding a new product (The .append method)
+# This adds an item to the very END of the list
+gpu_inventory.append("RTX 4090")
+
+print("\n--- After Product Launch ---")
+print(f"Updated Stock: {gpu_inventory}")
+
+# 3. Counting items (The len function)
+total_chips = len(gpu_inventory)
+print(f"We are now managing {total_chips} different product lines.")
