229fe8ace1902d6cf517e137d358302da5fce52f
Abhinav Shrivastav <your_email@example.com>
2026-01-05 01:22:05 +0530

    Add: confidence_builder01

---
+++ confidence_builder01.py
@@ -0,0 +1,14 @@
+#Created a Tuple
+camera_resolution = (1280,720)
+
+#Created a Set 
+detected_letter = {"A","B","A"}
+
+#Created a dictionary
+hand_info = {
+    "label": "Left Hand",
+    "resolution": camera_resolution,
+    "letters":detected_letter
+}
+
+print(hand_info)
